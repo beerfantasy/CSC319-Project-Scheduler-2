b1fb06011bb6b67720e861b6a935443d24b2b025
natnaree12 <natnaree.nat12@gmail.com>
2017-05-03 11:55:29 +0700

    Add file vis upload

---
+++ Task.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scheduler;
+
+/**
+ *
+ * @author Natnaree
+ */
+import java.util.*;
+public class Task {
+  private ArrayList activities = new ArrayList();
+
+  public Task() { }
+  
+  public int addActivity(Activity activity) {
+    activity.setID(activities.size());
+    activities.add(activity);
+    
+    return activity.getID();
+  }
+  public Activity getActivity(int id) {
+    Iterator iterator = activities.iterator();
+    Activity activity;
+    while(iterator.hasNext()) {
+      activity = (Activity) iterator.next();
+      if(activity.getID() == id)
+        return activity;
+    }
+    return null;
+  }
+  
+  public Iterator getActivities() { return activities.iterator();}
+}
